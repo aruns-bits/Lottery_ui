220d9e127656fac0009758d922bc2feb8169f259
arun sasthrampadi <arunsasthrampadi@aruns-MacBook-Pro-2.local>
2021-10-30 14:40:29 +0530

    ui app for lottery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+import './App.css';
+import web3 from "./web3.js";
+import lottery from "./lottery";
+
+class App extends Component {
+
+  state = {
+    "manager": "",
+    "players": [],
+    "balance": "",
+    "value": "",
+    "message": "",
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager: manager, players: players, balance: balance })
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    const Accounts = await web3.eth.getAccounts();
+    this.setState({ message: "Waiting on transaction success" });
+
+    await lottery.methods.enter().send({
+      from: Accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+
+    })
+
+    this.setState({ message: "You have been entered.." })
+  };
+  onClick = async (e) => {
+    const Accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success" })
+    await lottery.methods.pickWinner.send({
+      from: Accounts[0]
+    })
+    this.setState({ message: "A Winner has been picked" })
+  }
+
+  render() {
+    console.log(web3.version)
+    web3.eth.getAccounts()
+      .then(console.log)
+    return (
+      <div className="App">
+        <h2>Lottery Contract</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          There are currently {this.state.players.length} people entered.
+          Competing to win to {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+          This contract is managed by {this.state.manager}</p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck ?</h4>
+          <div>
+            <label>
+              Amout of ether to enter
+              </label>
+            <input value={this.state.value} onChange={e => {
+
+              this.setState({ value: e.target.value })
+            }} />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a Winner</button>
+        <hr />
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
